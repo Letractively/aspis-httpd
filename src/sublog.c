655c4de30dea7c310116ad0b314ed9b975f6f4a6
indugionefasto <indugionefasto@877f0d88-ce34-e9ba-efe8-6fdc8ea0a038>
2011-12-11 18:23:40 +0000

    open with close on exec

--- src/sublog.c
+++ src/sublog.c
@@ -106,7 +106,7 @@ int open_gen_fd(const char *spec)
         fd = open_net_fd(spec + 1);
     } else {
         fd = open(spec,
-                  O_WRONLY | O_CREAT | O_APPEND,
+                  O_WRONLY | O_CREAT | O_APPEND | O_CLOEXEC,
                   S_IRUSR | S_IWUSR | S_IROTH | S_IRGRP);
     }
     return fd;
